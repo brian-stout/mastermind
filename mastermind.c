3df419f9049ec9dfaf52a82a2c7c0c556652b265
Brian Stout <brianjonstout@gmail.com>
2016-10-28 11:31:52 -0400

    Broke all the stuff.  Implement a check to see if numbers were equal in the compare function but the loop logic is breaking down.  Need to fix.

--- mastermind.c
+++ mastermind.c
@@ -3,29 +3,35 @@
 #include <string.h>
 #include <time.h>
 
-void compare(int number[], char guess[]);
-void user_input(int number[]);
+
+int compare(int number[], char guess[]);
+int user_input(int number[]);
 
 int main(void)
 {
+	int guess_count = 0;
 	int number[4];
 	srand(time(NULL));
 	for(int i = 0; i < 4 ; ++i){
 		number[i] = rand() % 10;
 	}
 	printf("The number is %d%d%d%d \n", number[0],number[1],number[2],number[3]);
-	user_input(number);
+	guess_count = user_input(number);
+	printf("Congratulations!  You guessed the correct number in %d "
+		   "attempts!\n", guess_count);
 
 
 
 }
 
 
-void user_input(int number[])
+int user_input(int number[])
 {
 	char guess[8];
+	int cont = 1;
+	int guess_count = 0;
 	printf("Enter your guess: ");
-	while(1){
+	while(cont){
 		fgets(guess, sizeof(guess), stdin);
 		if(strlen(guess) == 5){
 			break;
@@ -33,28 +39,44 @@ void user_input(int number[])
 		else{
 			printf("Please enter a four digit number: ");
 		}
+		//Turns the unicode character to it's appropiate number
+		guess[4] = '\0';
+		for(int i = 0; i < 4; ++i){
+			guess[i] -= '0';
+		}
+		++guess_count; 
+		cont = compare(number, guess);
 	}
-	//Turns the unicode character to it's appropiate number
-	guess[4] = '\0';
-	for(int i = 0; i < 4; ++i){
-		guess[i] -= '0';
-	}
-	compare(number, guess);
+	return guess_count;
+
 
 }
 
 
-void compare(int number[], char guess[])
+int compare(int number[], char guess[])
 {
 	int red = 0;
 	int white = 0;
 	int mask[] = {0,0,0,0};
+	int cont;
+
+	//Tests to see if the numbers are equal before running other logic
+	for(int i = 0; i < 4; ++i){
+		if(number[i] != guess[i]){
+			cont = 1;
+		}
+		else{
+			cont = 0;
+		}
+	}
+	//Tests for reds, and set mask position to 1 to make that digit off limits
 	for(int i = 0; i < 4; ++i){
 		if(number[i] == guess[i]){
 			++red;
 			mask[i] = 1;
 		}
 	}
+	//Tests for whites
 	for(int i = 0; i < 4; ++i){
 		for(int j = 0; j < 4; ++j){
 			if((number[j] == guess[i]) && (mask[i] == 0)){
@@ -64,7 +86,8 @@ void compare(int number[], char guess[])
 	}
 	printf("%d red, ", red);
 	printf("%d white\n", white);
-	
+	return cont;
+
 }
 
 
